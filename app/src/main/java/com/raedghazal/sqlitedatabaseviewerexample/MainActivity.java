c923148f6c99dbb681c78edd1b07c75ae5daa88d
RaedGhazal <raed.osama078@gmail.com>
2018-11-21 00:31:55 +0200

    Activate Library

--- app/src/main/java/com/raedghazal/sqlitedatabaseviewerexample/MainActivity.java
+++ app/src/main/java/com/raedghazal/sqlitedatabaseviewerexample/MainActivity.java
@@ -6,14 +6,12 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.raedghazal.sqlite_database_viewer.RGDatabaseViewer;
-
 public class MainActivity extends AppCompatActivity {
 private DatabaseHelper databaseHelper = new DatabaseHelper(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new RGDatabaseViewer(this,databaseHelper).getView());
+        setContentView(R.layout.activity_main/*new RGDatabaseViewer(this,databaseHelper).getView()*/);
     }
     public class DatabaseHelper extends SQLiteOpenHelper{
         private DatabaseHelper(Context context) {
